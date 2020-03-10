6674e6b5dba5d85eb0eb431fe59600b88a7d2a3c
jc261936 <james.campbell1@my.jcu.edu.au>
2020-03-10 22:06:20 +1000

    Refactor password check program to use functions

--- Prac_03/password_check.py
+++ Prac_03/password_check.py
@@ -1,6 +1,19 @@
-min_length = 3
-password = input("Please enter a password with a minimum of 3 Characters- ")
-while len(password) < min_length:
+def main():
+    password = get_password()
+    make_asterisks(password)
+
+
+def make_asterisks(password):
+    stars = "*" * len(password)
+    print(stars)
+
+
+def get_password():
+    min_length = 3
     password = input("Please enter a password with a minimum of 3 Characters- ")
-stars = "*" * len(password)
-print(stars)
+    while len(password) < min_length:
+        password = input("Please enter a password with a minimum of 3 Characters- ")
+    return password
+
+
+main()
