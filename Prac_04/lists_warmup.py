aa835259a2f5ad9fdb589d1b0bbc9e7cb12e5077
jc261936 <james.campbell1@my.jcu.edu.au>
2020-03-17 14:31:09 +1000

    Write Python expressions

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+numbers[2:]
+9 in numbers
+
+"""Testing of each step"""
+print(numbers[0])
+print(numbers[-1])
+print(numbers[2:])
+print(numbers)
